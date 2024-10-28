ed6b0737fdec8e1628769c38a154668ce047b130
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-10-28 21:50:16 +0500

    Check expression form

--- Assets/_Project/Scripts/Maths/Wrapper/CommandValidationWrapper.cs
+++ Assets/_Project/Scripts/Maths/Wrapper/CommandValidationWrapper.cs
@@ -1,5 +1,7 @@
+using System.Text.RegularExpressions;
 using _Project.Scripts.Maths.Command;
 using _Project.Scripts.Maths.Strategy;
+using UnityEngine;
 
 namespace _Project.Scripts.Maths.Wrapper
 {
@@ -11,8 +13,19 @@ namespace _Project.Scripts.Maths.Wrapper
       _commandExecutionStrategy = commandExecutionStrategy;
 
     public bool TryExecuteExpression(string expression, out MathCommand command)
-    {
-      _commandExecutionStrategy.TrySolveExpression(expression, out command);
+    {     
+      string pattern = @"^\d+\+\d+$";
+        
+      bool isValid = Regex.IsMatch(expression, pattern);
+        
+      if (isValid)
+        _commandExecutionStrategy.TrySolveExpression(expression, out command);
+      else
+      {
+        command = null;
+        Debug.LogError("Expression form is not valid");
+      }
+
       return command != null;
     }
   }
