bcb382b4ac75caba37069db4baa6ca050a052e2b
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-10-30 11:46:32 +0500

    Describe asset provider code

--- Assets/_Project/Scripts/AssetProvider/AssetProviderInstaller.cs
+++ Assets/_Project/Scripts/AssetProvider/AssetProviderInstaller.cs
@@ -3,9 +3,12 @@ using VContainer;
 using VContainer.Unity;
 
 namespace _Project.Scripts.AssetProvider
-{
+{  
+  /// \class AssetProviderInstaller
+  /// \brief Класс отвечающий за инъекцию AssetProvider в приложение
   public class AssetProviderInstaller: IInstaller
-  {
+  {    
+    /// \brief Метод конфигурации зависимостей AssetProvider
     public void Install(IContainerBuilder builder) => 
       builder.Register<IAssetProvider, AssetProvider.Scripts.AssetProvider>(Lifetime.Singleton);
   }
--- Assets/_Project/Scripts/AssetProvider/Scripts/IAssetProvider.cs
+++ Assets/_Project/Scripts/AssetProvider/Scripts/IAssetProvider.cs
@@ -1,10 +1,18 @@
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.AssetProvider.Scripts
-{
+{  
+  /// \interface IAssetProvider
+  /// \brief Интерфейс, описывающий методы взаимодействия с поставщиком ресурсов
   public interface IAssetProvider
-  {
+  { 
+    /// \brief Метод загрузки ресурса
+    /// \param address Адресс/путь указывающий на объект ресурса, который нужно загрузить в память
+    /// \param T - тип ресурса
+    /// \return UniTask который можно подождать до завершения процесса запгрузки ресурса
     UniTask<T> Load<T>(string address) where T : class;
+    /// \brief Метод выгрузки ресурса
+    /// \param address Адресс/путь указывающий на объект ресурса, который нужно выгрузить из памяти
     void Release(string address);
   }
 }
